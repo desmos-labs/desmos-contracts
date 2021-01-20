0e50fa35ba829c0b743b9834985b5ede8a2dfb42
bragaz <leo.braga95@gmail.com>
2021-01-20 18:33:51 +0100

    updated CI finished to write integration tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -16,31 +16,36 @@
 //!      });
 //! 4. Anywhere you see query(&deps, ...) you must replace it with query(&mut deps, ...)
 
-use cosmwasm_vm::testing::{MockApi, MockQuerier, init, MOCK_CONTRACT_ADDR, mock_info, mock_env, mock_instance_options, MockStorage};
-use cosmwasm_vm::{Instance, Backend};
-use cosmwasm_std::{Coin, SystemResult, HumanAddr, attr};
-use desmos_contracts::msg::InitMsg;
-use desmos_contracts::state::state_read;
-use desmos_contracts::custom_query::DesmosQuery;
+use cosmwasm_std::{
+    attr, from_binary, Coin, ContractResult, Env, HandleResponse, HumanAddr, InitResponse,
+    MessageInfo, SystemResult,
+};
+use cosmwasm_storage::to_length_prefixed;
+use cosmwasm_vm::testing::{
+    handle, init, mock_env, mock_info, mock_instance_options, query, MockApi, MockQuerier,
+    MockStorage, MOCK_CONTRACT_ADDR,
+};
+use cosmwasm_vm::{Backend, Instance, Storage};
+use desmos_contracts::custom_query::{DesmosQuery, PostsQueryResponse};
 use desmos_contracts::mock::custom_query_execute;
+use desmos_contracts::msg::{HandleMsg, InitMsg, QueryMsg};
+use desmos_contracts::state::REPORTS_LIMIT_KEY;
+use desmos_contracts::types::Post;
 
 const WASM: &[u8] =
-include_bytes!("../target/wasm32-unknown-unknown/release/desmos_contracts.wasm");
+    include_bytes!("../target/wasm32-unknown-unknown/release/desmos_contracts.wasm");
 
-/*
 fn setup_test(
-    deps: &mut Backend<MockStorage, MockApi, MockQuerier>,
-    env: &Env,
+    deps: &mut Instance<MockApi, MockStorage, MockQuerier<DesmosQuery>>,
+    env: Env,
     info: MessageInfo,
     report_limit: u16,
 ) {
-    let init_msg = InitMsg{
-        reports_limit: report_limit
+    let init_msg = InitMsg {
+        reports_limit: report_limit,
     };
     let _res: InitResponse = init(deps, env.clone(), info, init_msg).unwrap();
 }
- */
-
 
 pub fn mock_dependencies_with_custom_querier(
     contract_balance: &[Coin],
@@ -68,13 +73,89 @@ fn test_init() {
 
     let init_msg = InitMsg { reports_limit: 5 };
 
-    let res = init(&mut deps, mock_env(), info, init_msg).unwrap();
+    let res: InitResponse = init(&mut deps, mock_env(), info, init_msg).unwrap();
     assert_eq!(0, res.messages.len());
 
     let exp_log = vec![attr("action", "set_default_reports_limit")];
     assert_eq!(res.attributes, exp_log);
 
     // make sure that the state is set
-    let state = state_read(&deps).load().unwrap();
-    assert_eq!(5, state.default_reports_limit)
+    deps.with_storage(|storage| {
+        let key = to_length_prefixed(REPORTS_LIMIT_KEY);
+        let data = storage.get(&key).0.unwrap().unwrap();
+        let default_limit = String::from_utf8(data).unwrap();
+
+        assert_eq!(default_limit, "{\"default_reports_limit\":5}");
+        Ok(())
+    })
+    .unwrap();
+}
+
+#[test]
+fn test_handle() {
+    let custom = mock_dependencies_with_custom_querier(&[]);
+    let instance_options = mock_instance_options();
+    let mut deps = Instance::from_code(WASM, custom, instance_options).unwrap();
+
+    let sender_addr = HumanAddr::from("addr0001");
+    let info = mock_info(&sender_addr, &[]);
+
+    setup_test(&mut deps, mock_env(), info.clone(), 3);
+
+    let exp_res = HandleResponse {
+        messages: vec![],
+        attributes: vec![
+            attr("action", "edit_reports_limit"),
+            attr("editor", info.sender.clone()),
+        ],
+        data: None,
+    };
+
+    let msg = HandleMsg::EditReportLimit { report_limit: 5 };
+    let res: ContractResult<HandleResponse> =
+        handle(&mut deps, mock_env(), info.clone(), msg.clone());
+
+    assert_eq!(res.unwrap(), exp_res);
+
+    let res: ContractResult<HandleResponse> =
+        handle(&mut deps, mock_env(), info.clone(), msg.clone());
+
+    assert!(res
+        .unwrap_err()
+        .contains("Report limit is the same as the stored one"));
+}
+
+#[test]
+fn query_filtered_posts_filter_correctly() {
+    let custom = mock_dependencies_with_custom_querier(&[]);
+    let instance_options = mock_instance_options();
+    let mut deps = Instance::from_code(WASM, custom, instance_options).unwrap();
+
+    let post = Post {
+        post_id: "id123".to_string(),
+        parent_id: "id345".to_string(),
+        message: "message".to_string(),
+        created: "date-time".to_string(),
+        last_edited: "date-time".to_string(),
+        allows_comments: false,
+        subspace: "subspace".to_string(),
+        optional_data: vec![],
+        attachments: vec![],
+        poll_data: vec![],
+        creator: "default_creator".to_string(),
+    };
+
+    let expected = PostsQueryResponse { posts: vec![post] };
+    let query_msg = QueryMsg::GetFilteredPosts { reports_limit: 3 };
+
+    // post has less reports than the limit
+    let res = query(&mut deps, mock_env(), query_msg).unwrap();
+    let unwrapped: PostsQueryResponse = from_binary(&res).unwrap();
+    assert_eq!(unwrapped, expected);
+
+    // post has equal reports to the limit
+    let query_msg = QueryMsg::GetFilteredPosts { reports_limit: 1 };
+    let res = query(&mut deps, mock_env(), query_msg).unwrap();
+    let unwrapped: PostsQueryResponse = from_binary(&res).unwrap();
+    assert_eq!(unwrapped, PostsQueryResponse { posts: vec![] })
 }
