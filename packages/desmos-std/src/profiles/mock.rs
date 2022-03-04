3ae1f856ea65baeda751d7ba470bea61e93ffd4a
Bax995 <leo.braga95@gmail.com>
2022-03-04 17:07:21 +0000

    fixed error in chain link struct

--- packages/desmos-std/src/profiles/mock.rs
+++ packages/desmos-std/src/profiles/mock.rs
@@ -90,6 +90,7 @@ impl MockProfilesQueries {
                 },
                 signature: Signature {
                     proto_type: "/desmos.profiles.v1beta1.SingleSignatureData".to_string(),
+                    mode: "SIGN_MODE_DIRECT".to_string(),
                     signature: "C7xppu4C4S3dgeC9TVqhyGN1hbMnMbnmWgXQI2WE8t0oHIHhDTqXyZgzhNNYiBO7ulno3G8EXO3Ep5KMFngyFg".to_string()
                 },
                 plain_text: "636f736d6f733138786e6d6c7a71727172367a74353236706e637a786536357a6b33663478676d6e6470786e32".to_string()
