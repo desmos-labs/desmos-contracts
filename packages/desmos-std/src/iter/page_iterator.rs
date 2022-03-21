3c840a4d57763655f83f3e7cc1d1a26eec10d272
Manuel Turetta <manuel.turetta94@gmail.com>
2022-03-21 12:04:01 +0000

    Updated the iterator to allow the usage of a generic key to fetch pages.

--- packages/desmos-std/src/iter/page_iterator.rs
+++ packages/desmos-std/src/iter/page_iterator.rs
@@ -1,38 +1,39 @@
 use cosmwasm_std::StdResult;
 
-/// Type alias of a function that fetch a page given as first value an
-/// offset and as second argument how many items to fetch from the given offset.
-pub type Fetcher<'a, T> = Box<dyn Fn(u64, u64) -> StdResult<Page<T>> + 'a>;
+/// Type alias of a function that fetch a page given as first argument an optional key
+/// that references the next page to fetch and as second argument how many items to fetch.
+/// If the first argument is None means that this function should fetch the first page.
+pub type Fetcher<'a, T, K> = Box<dyn Fn(Option<K>, u64) -> StdResult<Page<T, K>> + 'a>;
 
 /// A page of a elements.
-pub struct Page<T> {
+pub struct Page<T, K> {
     /// List of elements present in the page.
     pub items: Vec<T>,
+    /// Optional key to the next page to fetch.
+    /// If this is None means that there aren't other pages to fetch.
+    pub next_page_key: Option<K>,
 }
 
 /// Iterator that fetch paginated elements and allow to iterate over
 /// them as a continuous sequence of elements.
-pub struct PageIterator<'a, T: Clone> {
-    /// Function to fetch a page
-    fetch_page: Fetcher<'a, T>,
-    /// Optional cached page
-    current_page: Option<Page<T>>,
-    /// Position to the current element of the current page
+pub struct PageIterator<'a, T: Clone, K: Clone> {
+    /// Function to fetch a page.
+    fetch_page: Fetcher<'a, T, K>,
+    /// Optional cached page.
+    current_page: Option<Page<T, K>>,
+    /// Position to the current element of the current page.
     page_item_index: usize,
-    /// Current offset
-    offset: u64,
-    /// Size of each page
+    /// Size of each page.
     page_size: u64,
     /// Tells if the iterator has iterated over all the items.
     consumed: bool,
 }
 
-impl<'a, T: Clone> PageIterator<'a, T> {
+impl<'a, T: Clone, K: Clone> PageIterator<'a, T, K> {
     /// Creates a new iterator that fetch paginated items and allow to iterate over them
     /// as a continuous sequence of elements.
     ///
-    /// * `fetch_page` - Function that fetch a page given an offset and the amount of items to
-    /// fetch from the given offset
+    /// * `fetch_page` - Function that fetch the pages.
     /// * `page_size` - Size of each page.
     ///
     /// # Examples
@@ -40,98 +41,94 @@ impl<'a, T: Clone> PageIterator<'a, T> {
     /// ```
     /// use desmos_std::iter::page_iterator::{PageIterator, Page};
     ///
-    /// // Creates an iterator that return the numbers from 0 to 99.
-    /// let it = PageIterator::new(Box::new(|offset: u64, items: u64| {    ///
-    ///     Ok(Page {
-    ///         items: if offset >= 100 {
-    ///             Vec::new()
-    ///         } else {
-    ///             (offset..offset + items).collect()
-    ///         }
-    ///     })
-    /// }), 10);
+    /// // Creates an iterator that return the numbers from 0 to 19.
+    /// let it: PageIterator<u64, u64> = PageIterator::new(
+    ///            Box::new(|key, limit| {
+    ///                let start = key.unwrap_or(0);
+    ///                Ok(Page {
+    ///                    items: (start..start + limit).collect(),
+    ///                    next_page_key: if start + limit >= 20 {
+    ///                        None
+    ///                    } else {
+    ///                        Some(start + limit)
+    ///                    }
+    ///                })
+    ///            }),
+    ///            10,
+    ///        );
     /// ```
-    pub fn new(fetch_page: Fetcher<'a, T>, page_size: u64) -> PageIterator<'a, T> {
+    pub fn new(fetch_page: Fetcher<'a, T, K>, page_size: u64) -> PageIterator<'a, T, K> {
         PageIterator {
             fetch_page,
             current_page: None,
             page_item_index: 0,
-            offset: 0,
             page_size,
             consumed: false,
         }
     }
 }
 
-impl<'a, T: Clone> Iterator for PageIterator<'a, T> {
+impl<'a, T: Clone, K: Clone> Iterator for PageIterator<'a, T, K> {
     type Item = StdResult<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // If the iterator is consumed just return None.
+        // If the iterator is consumed just return None
         if self.consumed {
             return None;
         }
 
-        // If we have a page and we have iterate over all the items let's check
-        // if we need to fetch a new page or we have iterate over all the possible items
-        if let Some(page) = &self.current_page {
-            if page.items.len() == self.page_item_index {
-                // If the previously fetched page had less then
-                // page_size elements means there will not be another page
-                // to avoid fetch an empty page just mark the iterator as
-                // consumed and return None
-                if page.items.len() < self.page_size as usize {
-                    self.consumed = true;
-                    // Release the memory of the current page
-                    self.current_page = None;
-                    return None;
-                } else {
-                    // Clear the current page.
-                    self.current_page = None;
-                }
+        if self.current_page.is_none()
+            || self.current_page.as_ref().unwrap().items.len() == self.page_item_index
+        {
+            // Get the next page key
+            let next_key = self.current_page.as_ref().map_or(None, |page| {
+                page.next_page_key
+                    .as_ref()
+                    .map_or(None, |key| Some(key.clone()))
+            });
+
+            if next_key.is_none() && self.current_page.is_some() {
+                // We have fetched at least on page but there isn't a new page to fetch,
+                // to prevent an empty fetch just mark this iterator as consumed and return None
+                self.consumed = true;
+                self.current_page = None;
+                return None;
             }
-        }
 
-        match &self.current_page {
-            None => {
-                // No page available fetch a new one
-                let fetch_result: StdResult<Page<T>> =
-                    (self.fetch_page)(self.offset, self.page_size);
+            // Fetch a new page
+            let fetch_result: StdResult<Page<T, K>> = (self.fetch_page)(next_key, self.page_size);
 
-                match fetch_result {
-                    Ok(page) => {
-                        // No items on the new page so no more items, flag the iterator as consumed.
-                        if page.items.is_empty() {
-                            self.consumed = true;
-                            None
-                        } else {
-                            // Items available, update the offset field for the next fetch
-                            self.offset += page.items.len() as u64;
-                            // Clone the first item.
-                            let first_item = page.items[0].clone();
-                            // Save the fetched page
-                            self.current_page = Some(page);
-                            // Update the index for the next iteration.
-                            self.page_item_index = 1;
-                            // Return the first item of the new fetched page
-                            Some(Ok(first_item))
-                        }
-                    }
-                    // An error occurred, propagate it to the caller.
-                    Err(e) => {
-                        // Set the iterator as consumed to prevent other invocations.
+            match fetch_result {
+                Ok(page) => {
+                    // No items on the new page so no more items, flag the iterator as consumed
+                    if page.items.is_empty() {
                         self.consumed = true;
-                        Some(Err(e))
+                        None
+                    } else {
+                        // Clone the first item
+                        let first_item = page.items[0].clone();
+                        // Save the fetched page
+                        self.current_page = Some(page);
+                        // Update the index for the next iteration
+                        self.page_item_index = 1;
+                        // Return the first item of the new fetched page
+                        Some(Ok(first_item))
                     }
                 }
+                // An error occurred, propagate it to the caller
+                Err(e) => {
+                    // Set the iterator as consumed to prevent other invocations
+                    self.consumed = true;
+                    Some(Err(e))
+                }
             }
-            Some(page) => {
-                // A page is available, return the current item index
-                let result = Some(Ok(page.items[self.page_item_index].clone()));
-                // Update the iterator index.
-                self.page_item_index += 1;
-                result
-            }
+        } else {
+            // A page is available and we don't have iterated over all the items
+            let page = self.current_page.as_ref().unwrap();
+            let result = Some(Ok(page.items[self.page_item_index].clone()));
+            // Update the iterator index
+            self.page_item_index += 1;
+            result
         }
     }
 }
@@ -143,15 +140,17 @@ mod test {
 
     #[test]
     fn test_iterations_without_errors() {
-        let it = PageIterator::new(
-            Box::new(|offset, items| {
-                if offset == 20 {
-                    Ok(Page { items: Vec::new() })
-                } else {
-                    Ok(Page {
-                        items: (offset..offset + items).collect(),
-                    })
-                }
+        let it: PageIterator<u64, u64> = PageIterator::new(
+            Box::new(|key, limit| {
+                let start = key.unwrap_or(0);
+                Ok(Page {
+                    items: (start..start + limit).collect(),
+                    next_page_key: if start + limit >= 20 {
+                        None
+                    } else {
+                        Some(start + limit)
+                    },
+                })
             }),
             10,
         );
@@ -167,30 +166,47 @@ mod test {
 
     #[test]
     fn test_iterations_with_error() {
-        let mut it =
-            PageIterator::<i32>::new(Box::new(|_, _| Err(StdError::generic_err("ERROR :("))), 10);
+        let mut it = PageIterator::<i32, i32>::new(
+            Box::new(|_, _| Err(StdError::generic_err("ERROR :("))),
+            10,
+        );
 
         let item = it.next();
 
-        // First item should be some with the error that happen fetching the page.
+        // First item should be some with the error that happen fetching the page
         assert!(item.is_some());
         assert!(item.unwrap().is_err());
 
         // Since an error occurred when fetching the page the second item should be None
+        // to signal that the iterator don't have more items.
         let item = it.next();
         assert!(item.is_none());
     }
 
     #[test]
     fn test_iterations_with_partial_page() {
-        let it = PageIterator::new(Box::new(|_, _| Ok(Page { items: vec![1, 2] })), 10);
+        // Create an iterator that just return a page of 2 elements
+        // even if we requested a page of 10 elements.
+        let mut it: PageIterator<i32, i32> = PageIterator::new(
+            Box::new(|_, _| {
+                Ok(Page {
+                    items: vec![1, 2],
+                    next_page_key: None,
+                })
+            }),
+            10,
+        );
 
-        let mut it_counter = 0;
-        for element in it {
-            element.unwrap();
-            it_counter += 1;
-        }
+        // First items should be 1
+        let first = it.next();
+        assert_eq!(1, first.unwrap().unwrap());
 
-        assert_eq!(2, it_counter);
+        // Second items should be 1
+        let second = it.next();
+        assert_eq!(2, second.unwrap().unwrap());
+
+        // Third items should be None since we returned only 2 elements from the iterator.
+        let third = it.next();
+        assert_eq!(None, third)
     }
 }
