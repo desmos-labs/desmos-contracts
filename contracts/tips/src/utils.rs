32aabf6f452136c6e996beb77da97dab4ebe621b
Manuel <manuel.turetta94@gmail.com>
2022-09-07 11:13:52 +0200

    test: add test for merge_coins function
    jira: DCD-78

--- contracts/tips/src/utils.rs
+++ contracts/tips/src/utils.rs
@@ -1,24 +1,75 @@
 use cosmwasm_std::Coin;
 use std::collections::btree_map::BTreeMap;
 
-/// Iterates over the coins vector and merge the coins having the same `denom` value.
-/// * `coins` - Vector that will be iterated.
+/// Iterates over the coins vector and merge the coins having the same `denom.
+/// * `coins` - Vector of coins to merge.
 pub fn merge_coins(coins: Vec<Coin>) -> Vec<Coin> {
+    if coins.len() <= 1 {
+        return coins;
+    }
+
     let mut map: BTreeMap<String, u128> = BTreeMap::new();
     for coin in coins {
-        let value = map
-            .get(&coin.denom)
-            .map_or(coin.amount.u128(), |value| *value + coin.amount.u128());
-        map.insert(coin.denom, value);
+        let value = map.get_mut(&coin.denom);
+
+        if let Some(amount) = value {
+            *amount = *amount + coin.amount.u128();
+        } else {
+            map.insert(coin.denom, coin.amount.u128());
+        }
     }
 
     let mut coins: Vec<Coin> = Vec::with_capacity(map.len());
-    for entry in map {
+    for (denom, amount) in map.into_iter() {
         coins.push(Coin {
-            denom: entry.0,
-            amount: entry.1.into(),
+            denom,
+            amount: amount.into(),
         })
     }
 
     coins
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::merge_coins;
+    use cosmwasm_std::Coin;
+
+    #[test]
+    fn test_coin_merge_duplicates() {
+        let merged = merge_coins(vec![
+            Coin::new(100, "uatom"),
+            Coin::new(3000, "udsm"),
+            Coin::new(1000, "uosmo"),
+            Coin::new(200, "udsm"),
+            Coin::new(2000, "uatom"),
+        ]);
+
+        assert_eq!(
+            vec![
+                Coin::new(2100, "uatom"),
+                Coin::new(3200, "udsm"),
+                Coin::new(1000, "uosmo")
+            ],
+            merged
+        )
+    }
+
+    #[test]
+    fn test_coin_merge_no_duplicates() {
+        let merged = merge_coins(vec![
+            Coin::new(100, "uatom"),
+            Coin::new(3000, "udsm"),
+            Coin::new(1000, "uosmo"),
+        ]);
+
+        assert_eq!(
+            vec![
+                Coin::new(100, "uatom"),
+                Coin::new(3000, "udsm"),
+                Coin::new(1000, "uosmo")
+            ],
+            merged
+        )
+    }
 }
