9ca81fe594287a13d3c04fcb0f4883dea225bf8f
Paul <p22626262@gmail.com>
2022-10-07 17:23:55 +0800

    chore: expose remarkables execute msg schema

--- contracts/cw721-remarkables/examples/schema.rs
+++ contracts/cw721-remarkables/examples/schema.rs
@@ -2,7 +2,7 @@ use std::env::current_dir;
 use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema_with_title, remove_schemas, schema_for};
-
+use cw721_remarkables::ExecuteMsg;
 use cw721::{AllNftInfoResponse, NftInfoResponse};
 use cw721_remarkables::Metadata;
 
@@ -12,6 +12,7 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
+    export_schema_with_title(&schema_for!(ExecuteMsg), &out_dir, "ExecuteMsg");
     export_schema_with_title(
         &schema_for!(AllNftInfoResponse<Metadata>),
         &out_dir,
