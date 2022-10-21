5c2f9867545b3721e8477568d686f914c19e9952
Manuel <manuel.turetta94@gmail.com>
2022-10-21 16:11:53 +0200

    test: add contract unit tests
    jira: DCD-125

--- contracts/social-tips/src/contract.rs
+++ contracts/social-tips/src/contract.rs
@@ -191,4 +191,438 @@ fn query_user_pending_tips(
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::contract::{execute, instantiate, query};
+    use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, QueryPendingTipsResponse};
+    use crate::state::{PendingTip, PENDING_TIPS};
+    use crate::ContractError;
+    use cosmwasm_std::testing::{mock_env, mock_info};
+    use cosmwasm_std::{
+        from_binary, to_binary, Addr, BankMsg, Coin, DepsMut, Response, SubMsg, Uint64,
+    };
+    use desmos_bindings::mocks::mock_queriers::{
+        mock_desmos_dependencies, mock_desmos_dependencies_with_custom_querier, MockDesmosQuerier,
+    };
+    use desmos_bindings::msg::DesmosMsg;
+    use desmos_bindings::profiles::mocks::mock_profiles_query_response;
+    use desmos_bindings::profiles::models_app_links::{
+        ApplicationLink, ApplicationLinkOwnerDetails, ApplicationLinkState, CallData, Data,
+        OracleRequest,
+    };
+    use desmos_bindings::profiles::models_query::{
+        QueryApplicationLinkOwnersResponse, QueryApplicationLinksResponse,
+    };
+    use desmos_bindings::profiles::query::ProfilesQuery;
+    use desmos_bindings::query::DesmosQuery;
+
+    const ADMIN: &str = "admin";
+    const USER_1: &str = "user1";
+    const USER_2: &str = "user2";
+
+    fn init_contract(deps: DepsMut<DesmosQuery>) -> Result<Response<DesmosMsg>, ContractError> {
+        let info = mock_info(ADMIN, &[]);
+        let env = mock_env();
+        instantiate(deps, env, info, InstantiateMsg {})
+    }
+
+    #[test]
+    fn instantiate_properly() {
+        let mut deps = mock_desmos_dependencies();
+        init_contract(deps.as_mut()).unwrap();
+    }
+
+    #[test]
+    fn tip_with_empty_application_error() {
+        let mut deps = mock_desmos_dependencies();
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        let error = execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "".to_string(),
+                handler: "user".to_string(),
+            },
+        )
+        .unwrap_err();
+
+        assert_eq!(ContractError::InvalidApplication {}, error);
+    }
+
+    #[test]
+    fn tip_with_empty_handle_error() {
+        let mut deps = mock_desmos_dependencies();
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        let error = execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "".to_string(),
+            },
+        )
+        .unwrap_err();
+
+        assert_eq!(ContractError::InvalidUserHandler {}, error);
+    }
+
+    #[test]
+    fn tip_with_empty_funds_error() {
+        let mut deps = mock_desmos_dependencies();
+        let env = mock_env();
+        let info = mock_info(USER_1, &[]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        let error = execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "handle".to_string(),
+            },
+        )
+        .unwrap_err();
+
+        assert_eq!(ContractError::EmptyTipAmount {}, error);
+    }
+
+    #[test]
+    fn tip_user_linked_to_multiple_addresses_error() {
+        let querier = MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+            match profiler_query {
+                ProfilesQuery::ApplicationLinkOwners { .. } => {
+                    let response = QueryApplicationLinkOwnersResponse {
+                        owners: vec![
+                            ApplicationLinkOwnerDetails {
+                                user: Addr::unchecked(USER_1),
+                                application: "mocked_app".to_string(),
+                                username: "mocked_username".to_string(),
+                            },
+                            ApplicationLinkOwnerDetails {
+                                user: Addr::unchecked(USER_2),
+                                application: "mocked_app".to_string(),
+                                username: "mocked_username".to_string(),
+                            },
+                        ],
+                        pagination: None,
+                    };
+                    to_binary(&response).into()
+                }
+                _ => mock_profiles_query_response(profiler_query),
+            }
+        });
+
+        let mut deps = mock_desmos_dependencies_with_custom_querier(querier);
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        let error = execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "handle".to_string(),
+            },
+        )
+        .unwrap_err();
+
+        assert_eq!(ContractError::ToManyOwners {}, error);
+    }
+
+    #[test]
+    fn tip_collected_properly() {
+        let querier = MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+            match profiler_query {
+                ProfilesQuery::ApplicationLinkOwners { .. } => {
+                    let response = QueryApplicationLinkOwnersResponse {
+                        owners: vec![],
+                        pagination: None,
+                    };
+                    to_binary(&response).into()
+                }
+                _ => mock_profiles_query_response(profiler_query),
+            }
+        });
+
+        let mut deps = mock_desmos_dependencies_with_custom_querier(querier);
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "handle".to_string(),
+            },
+        )
+        .unwrap();
+
+        let pending_tips = PENDING_TIPS
+            .load(
+                &deps.storage,
+                ("application".to_string(), "handle".to_string()),
+            )
+            .unwrap();
+
+        assert_eq!(
+            vec![PendingTip {
+                sender: Addr::unchecked(USER_1),
+                amount: vec![Coin::new(10000, "udsm")],
+                block_height: 12345
+            }],
+            pending_tips
+        )
+    }
+
+    #[test]
+    fn tip_sent_properly() {
+        let querier = MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+            match profiler_query {
+                ProfilesQuery::ApplicationLinkOwners { .. } => {
+                    let response = QueryApplicationLinkOwnersResponse {
+                        owners: vec![ApplicationLinkOwnerDetails {
+                            user: Addr::unchecked(USER_2),
+                            application: "application".to_string(),
+                            username: "handle".to_string(),
+                        }],
+                        pagination: None,
+                    };
+                    to_binary(&response).into()
+                }
+                _ => mock_profiles_query_response(profiler_query),
+            }
+        });
+
+        let mut deps = mock_desmos_dependencies_with_custom_querier(querier);
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        let response = execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "handle".to_string(),
+            },
+        )
+        .unwrap();
+
+        assert_eq!(
+            &SubMsg::<DesmosMsg>::new(BankMsg::Send {
+                to_address: USER_2.to_string(),
+                amount: vec![Coin::new(10000, "udsm")],
+            }),
+            response.messages.first().unwrap()
+        )
+    }
+
+    #[test]
+    fn claim_no_pending_tips_error() {
+        let mut deps = mock_desmos_dependencies();
+        let env = mock_env();
+        let info = mock_info(USER_2, &[]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        let error = execute(deps.as_mut(), env, info, ExecuteMsg::ClaimTips {}).unwrap_err();
+
+        assert_eq!(
+            error,
+            ContractError::NoTipsAvailable {
+                user: USER_2.to_string()
+            }
+        )
+    }
+
+    #[test]
+    fn claim_pending_tip_properly() {
+        let querier = MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+            match profiler_query {
+                ProfilesQuery::ApplicationLinkOwners { .. } => {
+                    let response = QueryApplicationLinkOwnersResponse {
+                        owners: vec![],
+                        pagination: None,
+                    };
+                    to_binary(&response).into()
+                }
+                _ => mock_profiles_query_response(profiler_query),
+            }
+        });
+        let mut deps = mock_desmos_dependencies_with_custom_querier(querier);
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "handler".to_string(),
+            },
+        )
+        .unwrap();
+
+        // Mock USER_2 app link
+        deps.querier =
+            MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+                match profiler_query {
+                    ProfilesQuery::ApplicationLinks { .. } => {
+                        let response = QueryApplicationLinksResponse {
+                            links: vec![ApplicationLink {
+                                user: Addr::unchecked(USER_2),
+                                data: Data {
+                                    username: "handler".to_string(),
+                                    application: "application".to_string(),
+                                },
+                                state: ApplicationLinkState::VerificationSuccess,
+                                oracle_request: OracleRequest {
+                                    id: Uint64::new(0),
+                                    oracle_script_id: Uint64::new(0),
+                                    call_data: CallData {
+                                        application: "".to_string(),
+                                        call_data: "".to_string(),
+                                    },
+                                    client_id: "".to_string(),
+                                },
+                                result: None,
+                                creation_time: "".to_string(),
+                                expiration_time: "".to_string(),
+                            }],
+                            pagination: None,
+                        };
+                        to_binary(&response).into()
+                    }
+                    _ => mock_profiles_query_response(profiler_query),
+                }
+            });
+        let env = mock_env();
+        let info = mock_info(USER_2, &[Coin::new(10000, "udsm")]);
+
+        let response = execute(deps.as_mut(), env, info, ExecuteMsg::ClaimTips {}).unwrap();
+        assert_eq!(
+            &SubMsg::<DesmosMsg>::new(BankMsg::Send {
+                to_address: USER_2.to_string(),
+                amount: vec![Coin::new(10000, "udsm")],
+            }),
+            response.messages.first().unwrap()
+        );
+
+        // Ensure that the claimed tips have been deleted from the contract state
+        let pending_tips = PENDING_TIPS
+            .may_load(
+                &deps.storage,
+                ("application".to_string(), "handle".to_string()),
+            )
+            .unwrap();
+        assert_eq!(None, pending_tips);
+    }
+
+    #[test]
+    fn query_tips_properly() {
+        let querier = MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+            match profiler_query {
+                ProfilesQuery::ApplicationLinkOwners { .. } => {
+                    let response = QueryApplicationLinkOwnersResponse {
+                        owners: vec![],
+                        pagination: None,
+                    };
+                    to_binary(&response).into()
+                }
+                _ => mock_profiles_query_response(profiler_query),
+            }
+        });
+        let mut deps = mock_desmos_dependencies_with_custom_querier(querier);
+        let env = mock_env();
+        let info = mock_info(USER_1, &[Coin::new(10000, "udsm")]);
+
+        init_contract(deps.as_mut()).unwrap();
+
+        execute(
+            deps.as_mut(),
+            env,
+            info,
+            ExecuteMsg::SendTip {
+                application: "application".to_string(),
+                handler: "handler".to_string(),
+            },
+        )
+        .unwrap();
+
+        // Mock USER_2 app link
+        deps.querier =
+            MockDesmosQuerier::default().with_custom_profiles_handler(|profiler_query| {
+                match profiler_query {
+                    ProfilesQuery::ApplicationLinks { .. } => {
+                        let response = QueryApplicationLinksResponse {
+                            links: vec![ApplicationLink {
+                                user: Addr::unchecked(USER_2),
+                                data: Data {
+                                    username: "handler".to_string(),
+                                    application: "application".to_string(),
+                                },
+                                state: ApplicationLinkState::VerificationSuccess,
+                                oracle_request: OracleRequest {
+                                    id: Uint64::new(0),
+                                    oracle_script_id: Uint64::new(0),
+                                    call_data: CallData {
+                                        application: "".to_string(),
+                                        call_data: "".to_string(),
+                                    },
+                                    client_id: "".to_string(),
+                                },
+                                result: None,
+                                creation_time: "".to_string(),
+                                expiration_time: "".to_string(),
+                            }],
+                            pagination: None,
+                        };
+                        to_binary(&response).into()
+                    }
+                    _ => mock_profiles_query_response(profiler_query),
+                }
+            });
+
+        let response = query(
+            deps.as_ref(),
+            mock_env(),
+            QueryMsg::UserPendingTips {
+                user: USER_2.to_string(),
+            },
+        )
+        .unwrap();
+
+        let response: QueryPendingTipsResponse = from_binary(&response).unwrap();
+        assert_eq!(
+            response.tips,
+            vec![PendingTip {
+                sender: Addr::unchecked(USER_1),
+                amount: vec![Coin::new(10000, "udsm")],
+                block_height: 12345
+            }]
+        )
+    }
+}
