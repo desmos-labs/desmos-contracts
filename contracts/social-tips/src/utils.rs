2335f28e598472e8b5c8e5088510ab3dc28633c9
Manuel <manuel.turetta94@gmail.com>
2022-11-02 13:00:52 +0100

    test: add utils tests
    jira: DCD-125

--- contracts/social-tips/src/utils.rs
+++ contracts/social-tips/src/utils.rs
@@ -47,3 +47,95 @@ pub fn serialize_coins(coins: &[Coin]) -> String {
         .collect::<Vec<_>>()
         .join(",")
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::error::ContractError;
+    use crate::utils::{serialize_coins, sum_coins_sorted};
+    use cosmwasm_std::{Coin, OverflowError, OverflowOperation, StdError};
+
+    #[test]
+    fn test_coin_merge_duplicates_properly() {
+        let merged = sum_coins_sorted(vec![
+            Coin::new(100, "uatom"),
+            Coin::new(3000, "udsm"),
+            Coin::new(1000, "uosmo"),
+            Coin::new(200, "udsm"),
+            Coin::new(2000, "uatom"),
+        ])
+        .unwrap();
+
+        assert_eq!(
+            vec![
+                Coin::new(2100, "uatom"),
+                Coin::new(3200, "udsm"),
+                Coin::new(1000, "uosmo")
+            ],
+            merged
+        )
+    }
+
+    #[test]
+    fn test_coin_merge_no_duplicates_properly() {
+        let merged = sum_coins_sorted(vec![
+            Coin::new(100, "uatom"),
+            Coin::new(3000, "udsm"),
+            Coin::new(1000, "uosmo"),
+        ])
+        .unwrap();
+
+        assert_eq!(
+            vec![
+                Coin::new(100, "uatom"),
+                Coin::new(3000, "udsm"),
+                Coin::new(1000, "uosmo")
+            ],
+            merged
+        )
+    }
+
+    #[test]
+    fn test_coin_merge_overflow_error() {
+        let overflow_err = sum_coins_sorted(vec![
+            Coin::new(u128::MAX - 1, "uatom"),
+            Coin::new(3000, "uatom"),
+        ])
+        .unwrap_err();
+
+        assert_eq!(
+            ContractError::Std(StdError::overflow(OverflowError {
+                operation: OverflowOperation::Add,
+                operand1: (u128::MAX - 1).to_string(),
+                operand2: 3000.to_string(),
+            })),
+            overflow_err
+        )
+    }
+
+    #[test]
+    fn test_sort_coin_properly() {
+        let merged = sum_coins_sorted(vec![
+            Coin::new(100, "uosmo"),
+            Coin::new(1000, "uatom"),
+            Coin::new(1000, "udsm"),
+        ])
+        .unwrap();
+
+        assert_eq!(
+            vec![
+                Coin::new(1000, "uatom"),
+                Coin::new(1000, "udsm"),
+                Coin::new(100, "uosmo")
+            ],
+            merged
+        )
+    }
+
+    #[test]
+    fn serialize_coins_properly() {
+        assert_eq!(
+            "100uatom,100udsm",
+            serialize_coins(&[Coin::new(100, "uatom"), Coin::new(100, "udsm")])
+        )
+    }
+}
