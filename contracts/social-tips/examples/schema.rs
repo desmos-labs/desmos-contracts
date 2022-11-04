9d7126eac30ef4c97997c3ef5f36c8f983e9f3f6
Manuel <manuel.turetta94@gmail.com>
2022-11-04 14:42:18 +0100

    chore: fix contract schema
    jira: DCD-125

--- contracts/social-tips/examples/schema.rs
+++ contracts/social-tips/examples/schema.rs
@@ -1,10 +1,22 @@
-use cosmwasm_schema::write_api;
-use social_tips::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use cosmwasm_schema::{export_schema, remove_schemas};
+use schemars::schema_for;
+use social_tips::msg::{
+    ExecuteMsg, InstantiateMsg, QueryConfigResponse, QueryMsg, QueryPendingTipsResponse,
+    QueryUnclaimedSentTipsResponse,
+};
+use std::env::current_dir;
+use std::fs::create_dir_all;
 
 fn main() {
-    write_api! {
-        instantiate: InstantiateMsg,
-        execute: ExecuteMsg,
-        query: QueryMsg,
-    }
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(QueryPendingTipsResponse), &out_dir);
+    export_schema(&schema_for!(QueryUnclaimedSentTipsResponse), &out_dir);
+    export_schema(&schema_for!(QueryConfigResponse), &out_dir);
 }
